f5edfcde732716052559a6f53aa1dc2aa2c50492
prostoklim <113825978+prostoklim@users.noreply.github.com>
2022-09-25 16:31:08 +0700

    2 yrok
    очень жоско

---
+++ main.py
@@ -0,0 +1,42 @@
+# print("введи стоимость пирожка")
+# rub =int(input("Рубли: "))
+# cop =int(input("Копейки: "))
+# n = int(input("Кол-во пирожков: "))
+#
+# price_pie = ru#b * 100 + cop
+# need_to_pay = price_pie * n
+# print(f"С вас {need_to_pay // 100} рублей и {need_to_pay % 100} копеек")
+#
+# money = int(input("Рублев в кармане: "))
+# sdacha = money * 100 - need_to_pay
+# print(f"ваша сдача {sdacha // 100} рублей и {sdacha % 100} копеек")
+#
+# a = int(input("a: "))
+# if a % 2 == 1 :
+#     a = a + 1
+# else:
+#     a = a + 2
+#     print(a)
+#
+# r = int(input("Рад. окружности:"))
+# print(3.14 * r ** 2)
+#
+# n = int(input("Секунды: "))
+# hours = n // 3600
+# minutes = n // 60
+# if minutes >= n // 60:
+#     hours = hours + 1
+#     minutes = minutes - 60
+# seconds = n % 60
+# if hours < 10:
+#     hours = "0" + str(hours)
+#     if minutes < 10:
+#         minutes = "0" + str(minutes)
+# print(f"Время {hours}:{minutes}:{seconds}")
+
+# x = '''Строка'''
+# '''Строка 1 '''
+# '''Строка
+# x = "Что то"
+# x = 'Что то'
+# print("И он сказал: точка-точка-точка")
