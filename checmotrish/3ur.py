e9cf268c510ba8bc08722416e812713db3953ba1
movavi_school <klimnesterov8@gmail.com>
2022-10-02 16:23:42 +0700

    3урко

---
+++ checmotrish/3ur.py
@@ -0,0 +1,54 @@
+# # X = ["А", "Б", "В", "Г", "Д", "Е", "Ё", "Ж"]
+# # print("строка:", x)
+# # print("Последний элемент=", x[0])
+# # print("Последний элемент=", x[-1])
+# # print("Первый 5 элементов=",x[0:5])
+# # print("Первый 5 элементов через одного=", x[0:5:2])
+#
+#
+#
+# x = "Я строка, я карта, я, Антон"
+# print(x[-5:])
+# print(x[:8])
+# x = input ("Введи слово:")
+# temp = x[-1]
+# index = x.index(temp)
+# print(index + 1)
+# print(len(x))
+#
+# path = "C:/python3/python.exe"
+# print ("Имя файла:", path [-10:])
+# print ("Расширение:", path [-3:])
+# print ("Имя каталога:", path [3:10])
+# print ("Полный путь к каталогу:", path [0:9])
+#
+# path2= path.sprit("/")
+# print(path2)
+#
+# print ("Имя файла:", path [-1])
+# print ("Расширение:", path2 [-1:][-3:])
+# print ("Имя каталога:", path2 [2])
+# print (f"Полный путь к каталогу: {path2 []}/{path2[1]}")
+# chapter_1 = input("Глава 1:")
+# page_1 = input("Страница:")
+#
+# chapter_2 = input("Глава 2:")
+# page_2 = input("Страница:")
+#
+# chapter_3 = input("Глава 3:")
+# page_3 = input("Страница:")
+#
+# print(chapter_1.ljust(15) page_1.rjust(15))
+# print(chapter_2.ljust(15) page_2.rjust(15))
+# print(chapter_3.ljust(15) page_3.rjust(15))
+# x = "12`345`678`"
+# temp = x.split("`")
+# print(temp)
+# number = int(temp[0] + temp[1] + temp[2])
+# print(number)
+#
+# temp = x[0:2] +x[3:7] +[9:]
+# #number
+# print(temp)
+#
+# temp = x.replace()
