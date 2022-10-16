a576294cc6ea2c8317273dbae07166c6a2935812
movavi_school <uvukoska@gmail.com>
2022-10-16 16:31:28 +0700

    16 октября

--- lesson_4/main.py
+++ lesson_4/main.py
@@ -1,65 +1,65 @@
-# print("введи стоимость пирожка")
-# rub =int(input("Рубли: "))
-# cop =int(input("Копейки: "))
-# n = int(input("Кол-во пирожков: "))
+# # print("введи стоимость пирожка")
+# # rub =int(input("Рубли: "))
+# # cop =int(input("Копейки: "))
+# # n = int(input("Кол-во пирожков: "))
+# #
+# # price_pie = ru#b * 100 + cop
+# # need_to_pay = price_pie * n
+# # print(f"С вас {need_to_pay // 100} рублей и {need_to_pay % 100} копеек")
+# #
+# # money = int(input("Рублев в кармане: "))
+# # sdacha = money * 100 - need_to_pay
+# # print(f"ваша сдача {sdacha // 100} рублей и {sdacha % 100} копеек")
+# #
+# # a = int(input("a: "))
+# # if a % 2 == 1 :
+# #     a = a + 1
+# # else:
+# #     a = a + 2
+# #     print(a)
+# #
+# # r = int(input("Рад. окружности:"))
+# # print(3.14 * r ** 2)
+# #
+# # n = int(input("Секунды: "))
+# # hours = n // 3600
+# # minutes = n // 60
+# # if minutes >= n // 60:
+# #     hours = hours + 1
+# #     minutes = minutes - 60
+# # seconds = n % 60
+# # if hours < 10:
+# #     hours = "0" + str(hours)
+# #     if minutes < 10:
+# #         minutes = "0" + str(minutes)
+# # print(f"Время {hours}:{minutes}:{seconds}")
 #
-# price_pie = ru#b * 100 + cop
-# need_to_pay = price_pie * n
-# print(f"С вас {need_to_pay // 100} рублей и {need_to_pay % 100} копеек")
+# # x = '''Строка'''
+# # '''Строка 1 '''
+# # '''Строка
+# # x = "Что то"
+# # x = 'Что то'
+# # print("И он сказал: точка-точка-точка")
+# print("Привет")
+# alphabet="ФЬВГДЕЖЗ"
 #
-# money = int(input("Рублев в кармане: "))
-# sdacha = money * 100 - need_to_pay
-# print(f"ваша сдача {sdacha // 100} рублей и {sdacha % 100} копеек")
-#
-# a = int(input("a: "))
-# if a % 2 == 1 :
-#     a = a + 1
-# else:
-#     a = a + 2
-#     print(a)
-#
-# r = int(input("Рад. окружности:"))
-# print(3.14 * r ** 2)
-#
-# n = int(input("Секунды: "))
-# hours = n // 3600
-# minutes = n // 60
-# if minutes >= n // 60:
-#     hours = hours + 1
-#     minutes = minutes - 60
-# seconds = n % 60
-# if hours < 10:
-#     hours = "0" + str(hours)
-#     if minutes < 10:
-#         minutes = "0" + str(minutes)
-# print(f"Время {hours}:{minutes}:{seconds}")
-
-# x = '''Строка'''
-# '''Строка 1 '''
-# '''Строка
-# x = "Что то"
-# x = 'Что то'
-# print("И он сказал: точка-точка-точка")
-print("Привет")
-alphabet="ФЬВГДЕЖЗ"
-
-print(alphabet[:])
-# phrase="ктотто"
-# print(phrase.upper())
-#
-# phrase1="Яфраза, Яфраза, Яфраза"
-# print(phrase1.capitalize())
-fam = input("фам")
-name = input("имя")
-oth = input("отч")
-print(fam, name[0] +".", oth[0] + ".")
-# x = print()
-# print (x.count("a"))
-#
-# print (x.count("a"))
-email = ("Введи почту либо 1000-7")
-print(email.split("@"))
-login = email.split("@")[0]
-domain = email.split("@")[-1]
-print("Логин", login)
-print("Домэн", login)
+# print(alphabet[:])
+# # phrase="ктотто"
+# # print(phrase.upper())
+# #
+# # phrase1="Яфраза, Яфраза, Яфраза"
+# # print(phrase1.capitalize())
+# fam = input("фам")
+# name = input("имя")
+# oth = input("отч")
+# print(fam, name[0] +".", oth[0] + ".")
+# # x = print()
+# # print (x.count("a"))
+# #
+# # print (x.count("a"))
+# email = ("Введи почту либо 1000-7")
+# print(email.split("@"))
+# login = email.split("@")[0]
+# domain = email.split("@")[-1]
+# print("Логин", login)
+# print("Домэн", login)
