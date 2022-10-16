05e7d07bf72f5db32c08cf5bce09d1a70fcba0b6
movavi_school <uvukoska@gmail.com>
2022-10-16 16:31:28 +0700

    16 октября

---
+++ lesson_5/urok5.py
@@ -0,0 +1,64 @@
+# x = 7
+#
+# if x > 10:
+#     print("im do")
+#
+# else:
+#     print("and me")
+#
+#     phrase = "im compass"
+#     if phrase == "ya compass"
+#         print("We compass")
+#
+# game = "dota 2"
+# if game! = "brawl"
+# if x>= 10 and x == 0 or x == 666:
+#     print("ну выполнюсь хоть и ошибок нет")
+# else:
+#     print("Ну я тоже сработав")
+
+# number = int(input("введи число: "))
+# if number > 0:
+#     print("полож")
+# elif number == 0: #elif = else if (иначе если)
+#     print("ноль")
+# else:
+#     print("Отриц")
+
+# year = int(input("введи год:"))
+# if year % 4 == 0 and (year % 400 == or year % 100 !=0):
+#     print("високос")
+# else:
+#     print("невисокос:(")
+
+# number_1 = int(input())
+# operation = input("введи операцию(-,+)")
+# number_2 = int(input("введи второе число: "))
+# lst = ["-", "+"] #спиосок допустимых значений
+# if operation in lst: #если операция есть в списке
+#     if operation == "-": #если минус
+#         print(number_1 - number_2)
+#     elif operation == "+": #если плюс
+
+
+#         print(number_1 + number_2)
+# number_1 = int(input("Введи перове число:"))
+# number_2 = int(input("Введи второе число:"))
+# number_3 = int(input("Введи птретье число:"))
+#
+# counter_pol = 0 #счетчик положительных
+# counter_pol = 0 #счетчик отрицательных
+#
+# if number_1 < 0
+#     counter_otr += 1 #прибавить в отрицательным
+# else:
+#     counter_pol += 1 #прибавить к положительным
+#
+# #проверка третьего числа
+# if number_3
+# ticket = input("веди номер билета(6 знаков)")
+# if len(ticket) ==6
+#     first_half = ticket[3:] #сохраняем первые три числа
+#     last_half = ticket[3:] #сохраняем последние три чсила
+#
+#     first_sum = first_half [0] + first_half [1
