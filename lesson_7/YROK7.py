037b396ad42d568a0e21775adcc30ee538ee1c11
movavi_school <andrejcepurnoj3@gmail.com>
2022-10-30 16:35:03 +0700

    30 октября

---
+++ lesson_7/YROK7.py
@@ -0,0 +1,22 @@
+print("руко")
+# try:
+#     name=input("imia")
+#     if name == "a":
+#         raise Exception("NO")
+# except Excation as error_m: #xt
+#     print("fuk")
+# else: #inache
+#     print("nu lando")
+
+
+
+# try:
+#     number = int(input("hislo:"))
+#     x = 5 / number
+# excert Exception:
+#     pass #zatochka
+
+def summa(numbes):
+    return sum(numbers)
+
+print(summa([1, 2, 3])) #печатаем рез-т функцию при  [1, 2, 3]
