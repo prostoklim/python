e563eaefc54122defb72160c396df28c6c451415
movavi_school <lavr.ilyk@yandex.ru>
2022-11-06 16:28:04 +0700

    6 ноября

---
+++ lessson_9/main.py
@@ -0,0 +1,52 @@
+# import random
+# import time
+#
+# print("время умирать")
+# is_game = "y"
+# while is_game == "y"
+# time.sleep(0.5)
+# print("*заряжаем револьер*")
+# time.sleep(2.5)
+# print("*раскручиваем*")
+# time.sleep(1.5)
+# print(3)
+# time.sleep(1)
+# print(2)
+# time.sleep(1)
+# print(1)
+# time.sleep(1)
+# print("*Выстрел*")
+#
+# slots = [1, 2, 3, 4, 5, 6]
+# patron = random.choice(slots) # .choice - случайный выбор
+# our = random.choice(slots)
+#
+# if patron == our:
+#     print("СМЭРТЬ")
+
+
+# ЦИКЛЫ
+
+#for
+
+# lst = ["Антон1", "Антон2", "Антон3", "Антон4" ]
+# for antoha in lst:
+#     print(antoha, end="хз")
+#
+# for i in range(0,10):
+#     print("питон пофек")
+#
+# #while
+# x = 0
+# while x != 10:
+#     print(x)
+#     x += 1 #то же самое что и x = x + 1
+
+#
+# x = int(input("cifra:"))
+# step = 0
+# while x !=1:
+#     if x % 2 ==0:
+#         x = x // 2
+#     else:
+#         x = 3 * x + 1
