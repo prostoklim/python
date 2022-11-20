95baa8622a6da650e07bdc7118704b30827deb71
alexandroid103 <alexandr.s.ivanoff@gmail.com>
2022-11-20 16:22:38 +0700

    20 11 22

--- lesson_10/main.py
+++ lesson_10/main.py
@@ -1,44 +1,44 @@
-# lst = [0, 1, 2, 3, 4, 5]
-# for element in lst:
-#     print(element)
-#
-# for i in range(0, 10):
-#     print(i)
+lst = [0, 1, 2, 3, 4, 5]
+for element in lst:
+    print(element)
+
+for i in range(0, 10):
+    print(i)
 
 
 
-# x1 = int(input("Введи число:"))
-# x2 = int(input("Введи число:"))
-#
-# # через while
-# # while x1 <= x2:
-# #     print(x1)
-# #     x1 += 1  # то же самое что и x1 = x1 + 1
-#
-# # через for in range
-# for j in range(x1, x2 + 1):
-#     print(j)
+x1 = int(input("Введи число:"))
+x2 = int(input("Введи число:"))
+
+# через while
+# while x1 <= x2:
+#     print(x1)
+#     x1 += 1  # то же самое что и x1 = x1 + 1
+
+# через for in range
+for j in range(x1, x2 + 1):
+    print(j)
 
 
-# number = int(input("Ярусов: "))
-# while True:
-#     char = input("Символ: ")
-#     if len(char) == 1:  # len = length = длина
-#         break
-#
-# for i in range(1, number + 1):
-#     # левая половина
-#     print(" " * (number - i), end="")
-#     print(char * i, end="")
-#
-#     print(char * i)  # правая половина и перенос строки
+number = int(input("Ярусов: "))
+while True:
+    char = input("Символ: ")
+    if len(char) == 1:  # len = length = длина
+        break
 
-# x = int(input("Число: "))
-# for k in range(1, 21):
-#     print(x, "x", k, "=", x * k)
+for i in range(1, number + 1):
+    # левая половина
+    print(" " * (number - i), end="")
+    print(char * i, end="")
 
-# width = int(input("Ширина: "))
-# height = int(input("Высота: "))
-#
-# for i in range(0, height):
-#     print("# " * width)
+    print(char * i)  # правая половина и перенос строки
+
+x = int(input("Число: "))
+for k in range(1, 21):
+    print(x, "x", k, "=", x * k)
+
+width = int(input("Ширина: "))
+height = int(input("Высота: "))
+
+for i in range(0, height):
+    print("# " * width)
